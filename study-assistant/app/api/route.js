ea08786acb095bce8fee2a3cc117bc34f87b1239
smthom1 <smthom1111@gmail.com>
2023-05-20 14:11:50 -0700

    test

--- study-assistant/app/api/route.js
+++ study-assistant/app/api/route.js
@@ -1,5 +1,7 @@
-import {NextResponse} from 'next/server'
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({body: 'Hello, World!'})
+  return NextResponse.json({ body: "Hello, World!" });
 }
+
+//xx
