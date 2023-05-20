2e308cc5d658d0ecfd8e4fc20a4e6bcc6cca1c64
Bill Zhang <billzhangsc@gmail.com>
2023-05-20 11:32:27 -0700

    Server works

--- study-assistant/app/api/route.js
+++ study-assistant/app/api/route.js
@@ -1,5 +1,5 @@
 import {NextResponse} from 'next/server'
 
 export async function GET() {
-  return new NextResponse.json({message: 'Hello, World!'})
+  return NextResponse.json({body: 'Hello, World!'})
 }
