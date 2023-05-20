e6fcf6de70d4d1e0311c9fa56df0b425ee12519b
Bill Zhang <billzhangsc@gmail.com>
2023-05-20 16:25:20 -0700

    Works

--- study-assistant/app/api/stt/route.js
+++ study-assistant/app/api/stt/route.js
@@ -1,72 +1,72 @@
-import { NextRequest, NextResponse } from "next/server";
-const sdk = require("microsoft-cognitiveservices-speech-sdk");
-const util = require("util");
+import {NextRequest, NextResponse} from 'next/server'
+const sdk = require('microsoft-cognitiveservices-speech-sdk')
 
 export async function POST(req) {
-  // Assume body = the .wav file
   // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
   let speechConfig = sdk.SpeechConfig.fromSubscription(
     process.env.SPEECH_KEY,
-    process.env.SPEECH_REGION
-  );
-  speechConfig.speechRecognitionLanguage = "en-US";
+    process.env.SPEECH_REGION,
+  )
+  speechConfig.speechRecognitionLanguage = 'en-US'
+  const pushStream = sdk.AudioInputStream.createPushStream(
+    sdk.AudioStreamFormat.getWaveFormatPCM(16000, 16, 1),
+  )
 
-  const pushStream = sdk.AudioInputStream.createPushStream();
+  // Assume body = the .wav file
   for await (const chunk of req.body) {
-    pushStream.write(chunk);
+    pushStream.write(chunk.buffer)
   }
-  pushStream.close();
+  pushStream.close()
 
-  console.log(pushStream);
+  let audioConfig = sdk.AudioConfig.fromStreamInput(pushStream)
+  let recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig)
 
-  let audioConfig = sdk.AudioConfig.fromStreamInput(pushStream);
-  let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-
-  const resultPromise = new Promise((resolve, reject) => {
-    speechRecognizer.recognizeOnceAsync((result) => {
-      switch (result.reason) {
-        case sdk.ResultReason.RecognizedSpeech:
-          speechRecognizer.close();
-          resolve(NextResponse.json({ text: result.text }));
-          break;
-        case sdk.ResultReason.NoMatch:
-          speechRecognizer.close();
-          console.log("NOMATCH: Speech could not be recognized.");
-          resolve(
-            NextResponse({
-              status: 400,
-              body: "NOMATCH: Speech could not be recognized.",
-            })
-          );
-          break;
-        case sdk.ResultReason.Canceled:
-          const cancellation = sdk.CancellationDetails.fromResult(result);
-          console.log(`CANCELED: Reason=${cancellation.reason}`);
-
-          if (cancellation.reason == sdk.CancellationReason.Error) {
-            console.log(`CANCELED: ErrorCode=${cancellation.ErrorCode}`);
-            console.log(`CANCELED: ErrorDetails=${cancellation.errorDetails}`);
-            console.log(
-              "CANCELED: Did you update the speech key and location?"
-            );
+  let text = ''
+  let error = null
+  async function recognizeSpeech() {
+    return new Promise((resolve, reject) => {
+      recognizer.recognizeOnceAsync(
+        (result) => {
+          switch (result.reason) {
+            case sdk.ResultReason.RecognizedSpeech:
+              console.log(`RECOGNIZED: Text=${result.text}`)
+              text = result.text
+              resolve({
+                status: 200,
+                body: text,
+              })
+            case sdk.ResultReason.NoMatch:
+              error = 'NOMATCH: Speech could not be recognized.'
+              resolve({
+                status: 400,
+                body: error,
+              })
+            case sdk.ResultReason.Canceled:
+              const cancellation = sdk.CancellationDetails.fromResult(result)
+              if (cancellation.reason == sdk.CancellationReason.Error) {
+                error = `CANCELED: ErrorCode=${cancellation.ErrorCode}. ErrorDetails=${cancellation.errorDetails}`
+              } else {
+                error = `CANCELED: Reason=${cancellation.reason}`
+              }
+              resolve({
+                status: 400,
+                body: error,
+              })
           }
-          speechRecognizer.close();
-          resolve(
-            NextResponse({
-              status: 500,
-              body: "CANCELED: Speech could not be recognized.",
-            })
-          );
-          break;
-      }
-    });
-  });
-
-  try {
-    const response = await resultPromise;
-    return response;
-  } catch (err) {
-    console.log(err);
-    return NextResponse({ status: 500, body: err });
+          recognizer.close()
+        },
+        (error) => {
+          error = `Failed: ${error}`
+          recognizer.close()
+          resolve({
+            status: 400,
+            body: error,
+          })
+        },
+      )
+    })
   }
+
+  const response = await recognizeSpeech()
+  return NextResponse.json(response)
 }
